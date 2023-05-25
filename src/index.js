40d8cfaa4ad2aff7e177e2ed1fdfa6e1e94c8607
dev <remnev.dev@gmail.com>
2023-05-25 18:42:52 +0000

    feat: template card

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './styles/index.scss';
+import dataProducts from './scripts/dataProducts.js';
+const cardList = document.querySelector('.place-products__list');
+
+function createCard(item) {
+  const cardElement = document.querySelector('.card-template').content;
+  const card = cardElement.querySelector('.card').cloneNode(true);
+
+  const cardPhoto = card.querySelector('.card__photo');
+  const cardTitle = card.querySelector('.card__title');
+  const cardDescription = card.querySelector('.card__description');
+  const cardPrice = card.querySelector('.card__price');
+
+  cardPhoto.src = item.image;
+  cardTitle.textContent = item.title;
+  cardDescription.textContent = item.description;
+  // elementsPrice.value = item.price += ' ₽';
+  cardPrice.value = item.price;
+
+  card.querySelector('.card__button-delete').addEventListener('click', function (evt) {
+    evt.target.closest('.card').remove();
+  });
+
+  return card;
+}
+
+// функция добавления карточку на страницу
+function addCard(item, container) {
+  const newCard = createCard(item);
+  container.prepend(newCard);
+}
+
+// Карточки из коробки
+dataProducts.forEach(function (item) {
+  addCard(item, cardList);
+});
---
+++ src/scripts/dataProducts.js
@@ -0,0 +1,12 @@
+import image1 from '../img/img1.jpg';
+const dataProducts = [
+  {
+    title: 'Наименование товара',
+    image: image1,
+    description:
+      'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
+    price: '12300',
+  },
+];
+
+export default dataProducts;
